8e485561fdd9735eca69cfc99d44b0cfcecf727c
Christopher Diep <diep.christopher@gmail.com>
2017-11-20 21:09:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,8 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(katz_deli)
+  puts "Welcome, you are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
